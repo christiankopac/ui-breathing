1593105bb16609252eb2a4686b4414fff63c428f
Christian Kopac <hello@christiankopac.com>
2025-01-21 09:27:35 +0100

    Remove remix server

--- remix.config.js
+++ remix.config.js
@@ -5,5 +5,14 @@ export default {
     tailwind: true,
     serverDependenciesToBundle: [
       /^marked.*/,
+      /^p5.*/,  // Asegurarnos de que p5.js se empaquete correctamente
     ],
+    future: {
+      v2_dev: true,
+      v2_errorBoundary: true,
+      v2_headers: true,
+      v2_meta: true,
+      v2_normalizeFormMethod: true,
+      v2_routeConvention: true,
+    },
   };
