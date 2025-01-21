610aac7fcf74a3e677324be9b36569a40f769497
Christian Kopac <hello@christiankopac.com>
2025-01-21 09:38:59 +0100

    Netlify fix

--- remix.config.js
+++ remix.config.js
@@ -1,18 +1,32 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
     ignoredRouteFiles: ["**/.*"],
-    serverModuleFormat: "esm",
     tailwind: true,
     serverDependenciesToBundle: [
       /^marked.*/,
       /^p5.*/,  // Asegurarnos de que p5.js se empaquete correctamente
+      "p5",
+      "object-assign",
+      /^@remix-run\/.*/,
+      /^@netlify\/.*/,
+      "web-streams-polyfill",
+      "web-encoding",
+      "mrmime",
+      "data-uri-to-buffer",
+      "@web3-storage/multipart-parser",
     ],
     future: {
-      v2_dev: true,
-      v2_errorBoundary: true,
-      v2_headers: true,
-      v2_meta: true,
-      v2_normalizeFormMethod: true,
-      v2_routeConvention: true,
+      v3_fetcherPersist: true,
+      v3_lazyRouteDiscovery: true,
+      v3_relativeSplatPath: true,
+      v3_throwAbortReason: true,
+      v3_singleFetch: true,
     },
-  };
+    publicPath: "/build/",
+    assetsBuildDirectory: "public/build",
+    serverBuildPath: ".netlify/functions-internal/server.js",
+    serverMainFields: ["browser", "module", "main"],
+    serverModuleFormat: "cjs",
+    serverPlatform: "node",
+    serverMinify: false,
+};
